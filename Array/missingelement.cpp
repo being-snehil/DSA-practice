7409eed8381ae87cd666e3146d7898b75a1c7deb
Snehil Verma <snehilverma607@gmail.com>
2025-05-17 13:16:54 +0530

    missing element

---
+++ Array/missingelement.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+          vector<int>arr = { 1,2,3,5};
+          for(int j = 1; j <= 5; j++){ // <-- ye element hai 
+                    bool flag = false;
+                    for(int i = 0; i <= 4; i++){ // <-- ye index wala hai
+                              if(arr[i] == j){ // array ke andar ka element  == out element hua toh
+                                        flag = true;
+                                        break;
+                              }
+                    }
+                    if(flag == false) cout << j;
+          }
+}
